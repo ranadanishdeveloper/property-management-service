21e54f3326502cb175dba2929ad7405a426acf04
imranarif-hue <imran.arif@itechleadzllc.com>
2026-05-01 03:40:46 +0500

    New Logo added

--- storage/framework/views/590a1f8259726604f539c2d21c53835fab5ffa54.php
+++
@@ -1,3 +0,0 @@
-
-<?php echo $__env->yieldContent('content'); ?>
-<?php /**PATH /home/itechleadz-solutions/Pictures/Property Management System/main_file/resources/views/admin/content.blade.php ENDPATH**/ ?>
--- storage/framework/views/5f58f649d07334e7af5792e6c842583e6a8c52c6.php
+++
@@ -1,7 +0,0 @@
-<div class="card">
-    <div class="card-body">
-        <?php echo $page->content; ?>
-
-    </div>
-</div>
-<?php /**PATH /home/itechleadz-solutions/Pictures/Property Management System/main_file/resources/views/Pages/show.blade.php ENDPATH**/ ?>
