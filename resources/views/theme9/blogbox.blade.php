a5c68c1a66b7c7fb6aab6bc46a5339a501e1b2fb
imranarif-hue <imran.arif@itechleadzllc.com>
2026-05-20 20:47:06 +0500

    Completely remove login/signup buttons from theme6 header for custom domain

--- resources/views/theme9/blogbox.blade.php
+++ resources/views/theme9/blogbox.blade.php
@@ -30,7 +30,7 @@
                 </div>
                 <h3><a href="{{ route('custom.domain.blog.detail', ['slug' => $blog->slug]) }}">{{ $blog->title }}</a></h3>
                 <p class="blog-excerpt">{{ \Illuminate\Support\Str::limit(strip_tags($blog->content), 100) }}</p>
-                <a href="{{ $isCustomDomain ? route('custom.domain.property.detail', ['id' => \Crypt::encrypt($property->id)]) : route('property.detail', ['code' => $user->code, \Crypt::encrypt($property->id)]) }}" class="blog-readmore">Read More <i class="fas fa-arrow-right"></i></a>
+                <a href="{{ route('custom.domain.blog.detail', ['slug' => $blog->slug]) }}" class="blog-readmore">Read More <i class="fas fa-arrow-right"></i class="fas fa-arrow-right"></i></a>
             </div>
         </div>
     @empty
